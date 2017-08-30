2a8b0e37bc86b7aaea0d64b45fb8c8903d0ee3ab
Karolina <karolina.harecka@gmail.com>
2017-08-30 11:05:44 +0200

    commit ostateczny

--- js/main.js
+++ js/main.js
@@ -1,34 +1,19 @@
 "use strict";
 $(function() {
     function pobierzDane() {
-        $.getJSON("http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108", function(plikJson) {
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(plikJson) {
                
-
+            var nowyDiv=document.createElement("div");
+            $(nowyDiv).attr("id", "dane-programisty");
+            document.body.appendChild(nowyDiv);
            
-//           tworzenie znacznika p
-           var imie=document.createElement("p");
-           var zawod=document.createElement("p");
-            var nazwisko=document.createElement("p");
-            var firma=document.createElement("p");
-//           umiejscowienie znacznika p - powinno byc w divie o id dane programisty
-          
-            document.body.appendChild(imie);
-            document.body.appendChild(zawod);
-             document.body.appendChild(nazwisko);
-             document.body.appendChild(firma);
-//           wypełnienie znacznika p
-          
-            imie.innerHTML = "imie:" + plikJson.imie;
-            zawod.innerHTML = "zawod:" + plikJson.zawod;
-            nazwisko.innerHTML = "nazwisko:" + plikJson.nazwisko;
-            firma.innerHTML = "firma:" + plikJson.firma;
-//           jUserName.innerHTML = "tutaj zostanie wyswietlone userName:" + plikJson.userName;
-//           jUserUrl.innerHTML = "tutaj zostanie wyswietlone UserUrl:" + plikJson.userURL;
+            nowyDiv.innerHTML ="imie: " + plikJson.imie + "<br>zawod: " + plikJson.zawod + "<br>nazwisko:" + plikJson.nazwisko + "<br>firma: " + plikJson.firma;
+
        })
     }
         
 //    przypięcie funkcji do przycisku
-    $("#button").click(function(){
+    $("button").click(function(){
         pobierzDane();
     });
 });
